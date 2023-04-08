82c628ad18af9850e31b35a5180537cea738b4a1
ChangboBro <80183288+ChangboBro@users.noreply.github.com>
2023-04-08 23:26:21 +0800

    Update read_spl06.py

--- read_spl06.py
+++ read_spl06.py
@@ -1,15 +1,20 @@
-from machine import Pin,SoftI2C
+from machine import Pin,I2C
 import time
-from spl06 import SPL06
+#from spl06 import SPL06
+from SPL06_new import SPL06
 
-i2c=SoftI2C(scl=Pin(1),sda=Pin(0),freq=100_000)
-tmp=i2c.scan()
-print(tmp[0])
+i2c=I2C(0,sda=Pin(0),scl=Pin(1),freq=400_000)
 baro=SPL06(i2c)
+buff=baro.rListFReg(0x10,18)
+#buff=baro.read_reg(b'\x13',3)
+for each in buff:
+    print("0x%02X"%each,end=",")
 while(1):
+    tstamp=time.ticks_us()
     print("altitude %.4f"%baro.altitude())
-    print("compensated pressure:   %f"%baro.prse())
-    print("scaled raw pressure:    %f"%baro.prse(False))
-    print("scaled raw temperature: %f"%baro.sc_temp())
-    print("calibrate data:",baro.calist)
+    print("get alti time %d us"%(time.ticks_us()-tstamp))
+    print("compensated pressure:   %f"%baro.sampPres())
+    #print("compensated pressure:   %f"%baro.prse())
+    print("compensated temperature:%f"%baro.sampTemp())
+    #print("calibrate data:",baro.calist)
     time.sleep(2)
